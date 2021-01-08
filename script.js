e6868cada1b24f55bd0299098712e5d943495a00
Balazs Juhasz <juhaszbal@gmail.com>
2021-01-08 15:18:47 +0100

    implement speed change controls

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
@@ -103,6 +104,10 @@ const toggleMute = () => {
 
 // Change Playback Speed -------------------- //
 
+const changeSpeed = () => {
+  video.playbackRate = speed.value;
+};
+
 // Fullscreen ------------------------------- //
 
 // Event Listeners
@@ -113,3 +118,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
